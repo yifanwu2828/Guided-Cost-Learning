afe8849b469273af1553fcb9ad57bfe2741c5a59
Yifan Wu <yiw084@ucsd.edu>
2021-03-17 17:41:47 -0400

    evaluate

--- gcl/gcl/scripts/reward_visualization.py
+++ gcl/gcl/scripts/reward_visualization.py
@@ -1,16 +1,17 @@
-from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from sklearn.metrics import mean_squared_error
 import gym
 import gym_nav
-from stable_baselines3 import A2C, SAC, PPO
+from stable_baselines3 import PPO
 from tqdm import tqdm
 
-import pytorch_util as ptu
-import utils
-from collect_demo import evaluate
+
+def get_metrics(reward):
+    mean_reward = np.array(reward).mean()
+    std_reward = np.array(reward).std()
+    return mean_reward, std_reward
 
 
 def main():
@@ -33,13 +34,12 @@ if __name__ == '__main__':
     env.seed(0)
     obs = env.reset()
 
-    log_dict = {"act": [], "obs": [], "mlp_reward": [], "true_reward": []}
+    log_dict = {"act": [], "obs": [], "mlp_reward": [], "true_reward": [], }
     n_step = range(1000)
     for _ in tqdm(n_step):
         action, _states = model.predict(obs, deterministic=True)
-        log_dict["act"].append(action)
-
         obs, reward, done, info = env.step(action)
+        log_dict["act"].append(action)
         log_dict["obs"].append(obs)
         log_dict["mlp_reward"].append(float(reward_model(torch.from_numpy(obs).float(),
                                                          torch.from_numpy(action).float())
@@ -50,9 +50,14 @@ if __name__ == '__main__':
             obs = env.reset()
     env.close()
 
-    mlp_reward = np.array(log_dict["mlp_reward"])+1# TODO: original plot becomes similar after adding 1
+    mlp_reward = np.array(log_dict["mlp_reward"]) + 1  # TODO: original plot becomes similar after adding 1
     true_reward = np.array(log_dict["true_reward"])
-    print(mean_squared_error(true_reward, mlp_reward))
+
+    mean_mlp_reward, std_mlp_reward = get_metrics(mlp_reward)
+    mean_true_reward, std_true_reward = get_metrics(true_reward)
+    print(f"mean_mlp_reward:{mean_mlp_reward:.4f}, std_mlp_reward:{std_mlp_reward:.4f}")
+    print(f"mean_true_reward:{mean_true_reward:.4f}, std_true_reward:{std_true_reward:.4f}")
+    print(f"MSE: {mean_squared_error(true_reward, mlp_reward):.5f}")
 
     f, ax = plt.subplots()
     ax.scatter(range(mlp_reward.size), mlp_reward, label="mlp_reward")
@@ -61,7 +66,7 @@ if __name__ == '__main__':
     plt.show()
 
     f, ax = plt.subplots()
-    ax.scatter(range(mlp_reward.size), mlp_reward-1, label="mlp_reward")
+    ax.scatter(range(mlp_reward.size), mlp_reward - 1, label="mlp_reward")
     ax.scatter(range(true_reward.size), true_reward, label="true_reward")
     ax.legend()
     plt.show()
