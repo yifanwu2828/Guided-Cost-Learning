f8a5b7b908f56ad91d410be68f78b3fe10c49a73
Yifan Wu <yiw084@ucsd.edu>
2021-03-23 21:25:22 -0400

    visual use expert policy

--- gcl/gcl/scripts/reward_visual.py
+++ gcl/gcl/scripts/reward_visual.py
@@ -49,7 +49,8 @@ if __name__ == '__main__':
     policy_model = torch.load(fname2)
     policy_model.eval()
 
-    model = PPO.load("tmp/demo_agent/ppo_nav_env")
+    visual_model = PPO.load("tmp/demo_agent/ppo_nav_env")
+    model = PPO.load("ppo_nav_env")
     toc(start_load, "Loading")
     #######################################################################################
     # Init ENV
@@ -64,7 +65,7 @@ if __name__ == '__main__':
         obs = env.reset()
         n_step = range(500)
         for _ in tqdm(n_step):
-            action, _states = model.predict(obs, deterministic=True)
+            action, _states = visual_model.predict(obs, deterministic=True)
             obs, reward, done, info = env.step(action)
             reward_log_dict["act"].append(action)
             reward_log_dict["obs"].append(obs)
@@ -94,13 +95,45 @@ if __name__ == '__main__':
         print(f"mean_true_reward:{mean_true_reward:.4f}, std_true_reward:{std_true_reward:.4f}")
         print(f"MSE: {mean_squared_error(true_reward, scaled_reward):.5f}")
     #######################################################################################
-    if VERBOSE:
-        # mean_mlp_reward, std_mlp_reward = get_metrics(mlp_reward)
-        # mean_true_reward, std_true_reward = get_metrics(true_reward)
-        # print(f"mean_mlp_reward:{mean_mlp_reward:.4f}, std_mlp_reward:{std_mlp_reward:.4f}")
-        # print(f"mean_true_reward:{mean_true_reward:.4f}, std_true_reward:{std_true_reward:.4f}")
-        # print(f"MSE: {mean_squared_error(true_reward, mlp_reward):.5f}")
+        # Init Param
+        reward_log_dict = {"act": [], "obs": [], "mlp_reward": [], "true_reward": [], }
+        #######################################################################################
+        '''TEST LEARNING REWARD'''
+        if VISUAL:
+            obs = env.reset()
+            n_step = range(500)
+            for _ in tqdm(n_step):
+                action, _states = model.predict(obs, deterministic=True)
+                obs, reward, done, info = env.step(action)
+                reward_log_dict["act"].append(action)
+                reward_log_dict["obs"].append(obs)
+                reward_log_dict["mlp_reward"].append(float(reward_model(torch.from_numpy(obs).float(),
+                                                                        torch.from_numpy(action).float())
+                                                           .detach().numpy()))
+                reward_log_dict["true_reward"].append(reward)
+                # env.render()
+                if done:
+                    obs = env.reset()
+            env.close()
 
+            mlp_reward = np.array(reward_log_dict["mlp_reward"])
+            true_reward = np.array(reward_log_dict["true_reward"])
+
+            scaler = preprocessing.MinMaxScaler(feature_range=(-1, 0))
+            scaler.fit(mlp_reward.reshape(-1, 1))
+            scaled_reward = scaler.transform(mlp_reward.reshape(-1, 1))
+            f, ax = plt.subplots()
+            ax.scatter(range(mlp_reward.size), scaled_reward, label="mlp_reward")
+            ax.scatter(range(true_reward.size), true_reward, label="true_reward")
+            ax.legend()
+            plt.show()
+            mean_mlp_reward, std_mlp_reward = get_metrics(scaled_reward)
+            mean_true_reward, std_true_reward = get_metrics(true_reward)
+            print(f"mean_mlp_reward:{mean_mlp_reward:.4f}, std_mlp_reward:{std_mlp_reward:.4f}")
+            print(f"mean_true_reward:{mean_true_reward:.4f}, std_true_reward:{std_true_reward:.4f}")
+            print(f"MSE: {mean_squared_error(true_reward, scaled_reward):.5f}")
+        #######################################################################################
+    if VERBOSE:
         f1, ax1 = plt.subplots()
         ax1.scatter(range(mlp_reward.size), mlp_reward, label="mlp_reward")
         ax1.scatter(range(true_reward.size), true_reward, label="true_reward")
@@ -134,22 +167,22 @@ if __name__ == '__main__':
                 obs = np.concatenate((obs, np.zeros(env.vel_dim)))
                 rew = reward_model(torch.from_numpy(obs).float(), torch.from_numpy(a).float()).detach().numpy()
                 rew_lst.append(rew[0])
-                Z[i, j] = rew
+                Z[i, j] = rew[0]
                 # Z[i, j] = rew
                 # env.eval_gaussian()
                 # print(Z[i, j].shape)
                 # print(Z[i, j])
 
-        # scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1))
-        # scaler.fit(Z)
-        # Z = (scaler.transform(Z)*255).astype(np.uint8)
-        # print(rew_lst)
-        reward_min, reward_max = np.min(Z), np.max(Z)
-        print(reward_min, reward_max)
-        Z = ((Z - reward_min) / (reward_max - reward_min) * 255).astype(np.uint8)
-        print(Z)
-        reward_map = np.stack((Z, Z, Z), axis=-1)
-        plt.imshow(reward_map)
+        scaler = preprocessing.MinMaxScaler(feature_range=(-1, 0))
+        scaler.fit(Z)
+        Z = (scaler.transform(Z)*255).astype(np.uint8)
+
+        # reward_min, reward_max = np.min(Z), np.max(Z)
+        # print(reward_min, reward_max)
+        # Z = ((Z - reward_min) / (reward_max - reward_min) * 255).astype(np.uint8)
+        # print(Z)
+        reward_map = Z  # np.stack((Z, Z, Z), axis=-1)
+        plt.imshow(reward_map, cmap='gray', vmin=0, vmax=255)
         plt.title("Learning Reward")
         plt.show()
 
