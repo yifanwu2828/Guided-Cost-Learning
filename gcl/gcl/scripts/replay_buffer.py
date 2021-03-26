1b4b73582b7e65c5a5657ab3c7289415fac56a87
Yifan Wu <yiw084@ucsd.edu>
2021-03-26 19:30:49 -0400

    add docstring, assertion, type hint

--- gcl/gcl/scripts/replay_buffer.py
+++ gcl/gcl/scripts/replay_buffer.py
@@ -1,15 +1,13 @@
+import numpy as np
+from typing import List, Tuple
 import utils
-import numpy as np 
-from typing import List
+from utils import PathDict
 
 
 class ReplayBuffer(object):
 
     def __init__(self, max_size=1000000):
-        default_size = 1000000
-        if max_size > default_size:
-            print("###########################")
-            print(f"Exceed default_size: {default_size}")
+
         self.max_size = max_size
         # store each rollout
         self.paths = []
@@ -27,15 +25,22 @@ class ReplayBuffer(object):
     def __len__(self):
         return len(self.paths)
 
-    def add_rollouts(self, paths):
+    def add_rollouts(self, paths: List[PathDict]):
         """ Add new rollouts into our list of rollouts """
+        assert len(paths) > 0, "Adding empty rollout"
         self.paths.extend(paths)
+        if len(self.paths) > self.max_size:
+            print("###########################")
+            print(f"Exceed buffer max_size {self.max_size} by {len(self.paths) - self.max_size}, old path will be "
+                  f"dropped")
         # if size exceed buffer's max size, drop old rollouts and keep new on instead
         self.paths = self.paths[-self.max_size:]
         self.num_paths =len(self.paths)
 
         # convert new rollouts into their component arrays, and append them onto our arrays
-        observations, actions, log_probs, next_observations, terminals, concatenated_rews, unconcatenated_rews = utils.convert_listofrollouts(paths)
+        (observations, actions, log_probs,
+         next_observations, terminals,
+         concatenated_rews, unconcatenated_rews) = utils.convert_listofrollouts(paths)
 
         if self.obs is None:
             self.obs = observations[-self.max_size:]
@@ -46,61 +51,89 @@ class ReplayBuffer(object):
             self.concatenated_rews = concatenated_rews[-self.max_size:]
             self.unconcatenated_rews = unconcatenated_rews[-self.max_size:]
         else:
+            # keep only latest max_size around
             self.obs = np.concatenate([self.obs, observations])[-self.max_size:]
             self.acs = np.concatenate([self.acs, actions])[-self.max_size:]
             self.log_probs = np.concatenate([self.log_probs, log_probs])[-self.max_size:]
-            self.next_obs = np.concatenate(
-                [self.next_obs, next_observations]
-            )[-self.max_size:]
-            self.terminals = np.concatenate(
-                [self.terminals, terminals]
-            )[-self.max_size:]
-            self.concatenated_rews = np.concatenate(
-                [self.concatenated_rews, concatenated_rews]
-            )[-self.max_size:]
+            self.next_obs = np.concatenate([self.next_obs, next_observations])[-self.max_size:]
+            self.terminals = np.concatenate([self.terminals, terminals])[-self.max_size:]
+            self.concatenated_rews = np.concatenate([self.concatenated_rews, concatenated_rews])[-self.max_size:]
             if isinstance(unconcatenated_rews, list):
-                self.unconcatenated_rews += unconcatenated_rews  # TODO keep only latest max_size around
+                self.unconcatenated_rews += unconcatenated_rews
             else:
-                self.unconcatenated_rews.append(unconcatenated_rews)  # TODO keep only latest max_size around
+                self.unconcatenated_rews.append(unconcatenated_rews)
         self.num_data = self.obs.shape[0]
+
     ########################################
     ########################################
 
     def sample_random_rollouts(self, num_rollouts) -> np.ndarray:
-        """ Randomly select Rollouts """
+        """
+        Randomly select Rollouts
+        :param: num_rollouts
+        :type: int
+        :return: random rollouts
+        :type: np.array(List[PathDict])
+        """
+        assert isinstance(num_rollouts, int)
+        assert len(self.paths) != 0, "No rollouts in Buffer"
         assert len(self.paths) >= num_rollouts, "Rollouts in Buffer is less than rollouts acquired "
         rand_indices = np.random.permutation(len(self.paths))[:num_rollouts]
         return np.array(self.paths)[rand_indices]
 
-    def sample_recent_rollouts(self, num_rollouts=1)->List:
-        """ Select Recent Rollouts """
-        assert len(self.paths)>=num_rollouts, "Rollouts in Buffer is less than rollouts acquired "
+    def sample_recent_rollouts(self, num_rollouts: int = 1) -> List:
+        """
+        Select Recent Rollouts
+        :param: num_rollouts
+        :type: int
+        :return: recent n rollouts
+        :type: List[PathDict]
+        """
+        assert isinstance(num_rollouts, int)
+        assert len(self.paths) != 0, "No rollouts in Buffer"
+        assert len(self.paths) >= num_rollouts, "Rollouts in Buffer is less than rollouts acquired "
         # return np.array(self.paths)[-num_rollouts:]
         return np.array(self.paths)[-num_rollouts:].tolist()
 
     def sample_all_rollouts(self) -> np.ndarray:
-        """ Return All Rollouts from Buffer """
+        """
+        Return All Rollouts from Buffer
+        :return: random rollouts
+        :type: np.array(List[PathDict])
+        """
         assert len(self.paths)!= 0, "No rollouts in Buffer"
         return np.array(self.paths)
 
     ########################################
     ########################################
-    def sample_random_data(self, batch_size):
+
+    def sample_random_data(self, batch_size: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
         Sample random transition steps of size batch_size
         """
+        assert isinstance(batch_size, int)
+        assert len(self.paths) != 0, "No path in Buffer"
+        assert 0 <= batch_size <= self.obs.shape[0], "No enough transition steps in buffer"
         assert (self.obs.shape[0] == self.acs.shape[0]
                 == self.concatenated_rews.shape[0]
-                == self.next_obs.shape[0] == self.terminals.shape[0])
+                == self.next_obs.shape[0] == self.terminals.shape[0]), "num of data do not match"
+
         rand_indices = np.random.permutation(self.obs.shape[0])[:batch_size]
         return (self.obs[rand_indices], self.acs[rand_indices], self.concatenated_rews[rand_indices],
                 self.next_obs[rand_indices], self.terminals[rand_indices])
 
-    def sample_recent_data(self, batch_size=1, concat_rew=True):
+    def sample_recent_data(self,
+                           batch_size: int = 1,
+                           concat_rew=True
+                           ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
         Sample recent transition steps of size batch_size
+        :param batch_size: num of recent transition steps
+        :param concat_rew:
+        :return: observations, actions, unconcatenated_rews, next_observations, terminals
         """
-        assert len(self.paths) != 0, "No Data in Buffer"
+        assert isinstance(batch_size, int)
+        assert len(self.paths) != 0, "No recent Data in Buffer"
         assert self.num_data >= batch_size, "Data in Buffer is less than data acquired "
         if concat_rew:
             return (self.obs[-batch_size:], self.acs[-batch_size:],
