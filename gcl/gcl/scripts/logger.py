3be1929e4851a66bce34107d65e2a3227210b671
Yifan Wu <yiw084@ucsd.edu>
2021-03-29 05:43:22 -0400

    add property

--- gcl/gcl/scripts/logger.py
+++ gcl/gcl/scripts/logger.py
@@ -12,22 +12,30 @@ class Logger:
         self._n_logged_samples = n_logged_samples
         self._summ_writer = SummaryWriter(log_dir, flush_secs=1, max_queue=1)
 
-    def log_scalar(self, scalar, name, step_):
+    @property
+    def n_logged_samples(self):
+        return self._n_logged_samples
+
+    @property
+    def summ_writer(self):
+        return self._summ_writer
+
+    def log_scalar(self, scalar, name, step_) -> None:
         self._summ_writer.add_scalar('{}'.format(name), scalar, step_)
 
-    def log_scalars(self, scalar_dict, group_name, step, phase):
+    def log_scalars(self, scalar_dict, group_name, step, phase) -> None:
         """Will log all scalars in the same plot."""
         self._summ_writer.add_scalars('{}_{}'.format(group_name, phase), scalar_dict, step)
 
-    def log_image(self, image, name, step):
+    def log_image(self, image, name, step) -> None:
         assert(len(image.shape) == 3)  # [C=channel, H=height, W=width], Default is (3,H,W)
         self._summ_writer.add_image('{}'.format(name), image, step)
 
-    def log_video(self, video_frames, name, step, fps=10):
+    def log_video(self, video_frames, name, step, fps=10) -> None:
         assert len(video_frames.shape) == 5, "Need [N, T, C, H, W] input tensor for video logging!"
         self._summ_writer.add_video('{}'.format(name), video_frames, step, fps=fps)
 
-    def log_paths_as_videos(self, paths, step, max_videos_to_save=2, fps=10, video_title='video'):
+    def log_paths_as_videos(self, paths, step, max_videos_to_save=2, fps=10, video_title='video') -> None:
 
         # reshape the rollouts
         videos = [np.transpose(p['image_obs'], [0, 3, 1, 2]) for p in paths]
@@ -49,23 +57,23 @@ class Logger:
         videos = np.stack(videos[:max_videos_to_save], 0)
         self.log_video(videos, video_title, step, fps=fps)
 
-    def log_figures(self, figure, name, step, phase):
+    def log_figures(self, figure, name, step, phase) -> None:
         """figure: matplotlib.pyplot figure handle"""
         assert figure.shape[0] > 0, "Figure logging requires input shape [batch x figures]!"
         self._summ_writer.add_figure('{}_{}'.format(name, phase), figure, step)
 
-    # def log_figure(self, figure, name, step, phase):
-    #     """figure: matplotlib.pyplot figure handle"""
-    #     self._summ_writer.add_figure('{}_{}'.format(name, phase), figure, step)
+    def log_figure(self, figure, name, step, phase) -> None:
+        """figure: matplotlib.pyplot figure handle"""
+        self._summ_writer.add_figure('{}_{}'.format(name, phase), figure, step)
 
     # def log_graph(self, array, name, step, phase):
     #     """figure: matplotlib.pyplot figure handle"""
     #     im = plot_graph(array)
     #     self._summ_writer.add_image('{}_{}'.format(name, phase), im, step)
 
-    def dump_scalars(self, log_path=None):
+    def dump_scalars(self, log_path=None) -> None:
         log_path = os.path.join(self._log_dir, "scalar_data.json") if log_path is None else log_path
         self._summ_writer.export_scalars_to_json(log_path)
 
-    def flush(self):
+    def flush(self) -> None:
         self._summ_writer.flush()
