c6e5abd936f727179401ee8d0e2b226168a40917
Yifan Wu <yiw084@ucsd.edu>
2021-03-28 22:23:38 -0400

    add __repr__

--- gcl/gcl/agents/base_agent.py
+++ gcl/gcl/agents/base_agent.py
@@ -5,6 +5,9 @@ class BaseAgent(object, metaclass=abc.ABCMeta):
     def __init__(self, **kwargs):
         super(BaseAgent, self).__init__(**kwargs)
 
+    def __repr__(self) -> str:
+        return f"BaseAgent"
+
     def train(self) -> dict:
         """Return a dictionary of logging information."""
         raise NotImplementedError
