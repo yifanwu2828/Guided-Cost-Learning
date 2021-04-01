1de1418c2c2056876bd1dc109d2d99b32fdf96f6
Yifan Wu <yiw084@ucsd.edu>
2021-04-01 03:43:53 -0400

    test with two agents

--- gcl/gcl/scripts/test_env.py
+++ gcl/gcl/scripts/test_env.py
@@ -6,42 +6,66 @@ import sklearn.preprocessing as preprocessing
 import gym
 import gym_nav
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
 from tqdm import tqdm
-
+import time
+from gym_nav.envs.multi_nav_env import MultiNavEnv
 from utils import tic, toc
 
 if __name__ == '__main__':
     #######################################################################################
-    # Set overflow from warning to raise
-    np.seterr(all='raise')
-    torch.autograd.set_detect_anomaly(True)
     #######################################################################################
     # Set seed
     SEED = 1
     np.random.seed(SEED)
     torch.random.manual_seed(SEED)
     #######################################################################################
-    # load model
+    # # load model
     start_load = tic("############ Load Model ############")
-    visual_model = PPO.load("tmp/demo_agent/ppo_nav_env")
-    model = PPO.load("ppo_nav_env")
+    demo_model = PPO.load("ppo_nav_env")
 
-    fname2 = "test_gcl_policy.pth"
+    fname2 = "test_gcl_policy_650.pth"
     policy_model = torch.load(fname2)
     policy_model.eval()
     #######################################################################################
     # Init ENV
-    env = gym.make('NavEnv-v0')
+    env = gym.make("MultiNavEnv-v0")
     env.seed(SEED)
     #######################################################################################
-    obs = env.reset()
+    demo_obs, agent_obs = env.reset()
     n_step = range(500)
     for t in tqdm(n_step):
-        action, _logprob = policy_model.get_action(obs)
-        # action, _states = model.predict(obs, deterministic=True)
+        demo_action, _ = demo_model.predict(demo_obs, deterministic=True)
+        agent_action, log_prob = policy_model.get_action(agent_obs)
+        agent_action = agent_action[0]
 
-        obs, reward, done, info = env.step(action[0])
+        # if demo_action[0] == agent_action[0] and demo_action[1] == agent_action[1]:
+        print(f"demo:{demo_action[0]}, {demo_action[1]}")
+        print(f"agent:{agent_action[0]}, {agent_action[1]}")
+
+
+        obs, reward, done, info = env.step(demo_action, agent_action)
+        demo_obs, agent_obs = obs
+        demo_rew, agent_rew = reward
+        demo_done, agent_done = done
         env.render()
+        done = any([demo_done, agent_done])
         if done:
-            obs = env.reset()
+            demo_obs, agent_obs = env.reset()
+            time.sleep(0.05)
     env.close()
+
+    # env = gym.make("NavEnv-v0")
+    # env.seed(SEED)
+    #
+    # demo_obs = env.reset()
+    # n_step = range(500)
+    # for t in tqdm(n_step):
+    #     demo_action, _states = demo_model.predict(demo_obs, deterministic=True)
+    #     print(demo_action)
+    #     demo_obs, reward, done, info = env.step(demo_action)
+    #     env.render()
+    #     if done:
+    #         demo_obs = env.reset()
+    #         time.sleep(0.05)
+    # env.close()
